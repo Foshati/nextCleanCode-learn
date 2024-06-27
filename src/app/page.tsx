8494fd555c95dc5ab96a23ef567d8c2d06f4665b
Foshati <amirrezafoshati@gmail.com>
2024-06-27 16:59:33 +0330

    test4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  useState();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
